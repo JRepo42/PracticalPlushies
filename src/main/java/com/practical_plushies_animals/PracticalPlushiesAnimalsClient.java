fd180ea0a8ae5032dfe06af837b4c27a7569d85d
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-01 18:44:11 +0000

    add(client mod initialiser)

---
+++ src/main/java/com/practical_plushies_animals/PracticalPlushiesAnimalsClient.java
@@ -0,0 +1,10 @@
+package com.practical_plushies_animals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PracticalPlushiesAnimalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

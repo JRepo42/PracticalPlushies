5ced83b82d3951a3011d8f02ca5eee72286e2cd3
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-01 15:53:11 +0000

    add(cooking up a hashmap system for automatic loot table registry)

--- src/main/java/com/practical_plushies_animals/datagen/PlushieLootTables.java
+++ src/main/java/com/practical_plushies_animals/datagen/PlushieLootTables.java
@@ -1,15 +1,23 @@
 package com.practical_plushies_animals.datagen;
 
+import com.practical_plushies_animals.PracticalPlushiesAnimals;
+import com.practical_plushies_animals.registry.PlushieRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+
+import java.util.HashMap;
 
 public class PlushieLootTables extends FabricBlockLootTableProvider {
     protected PlushieLootTables(FabricDataOutput dataOutput) {
         super(dataOutput);
     }
 
+    public static void addDropEntries(HashMap plushieMap) {
+        PracticalPlushiesAnimals.LOGGER.info(plushieMap.toString());
+    }
+
     @Override
     public void generate() {
-
+        addDrop(PlushieRegistry.FOX_PLUSHIE, PlushieRegistry.FOX_PLUSHIE.asItem());
     }
 }
--- src/main/java/com/practical_plushies_animals/registry/PlushieRegistry.java
+++ src/main/java/com/practical_plushies_animals/registry/PlushieRegistry.java
@@ -1,6 +1,7 @@
 package com.practical_plushies_animals.registry;
 
 import com.practical_plushies_animals.PracticalPlushiesAnimals;
+import com.practical_plushies_animals.datagen.PlushieLootTables;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -9,16 +10,24 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import java.util.HashMap;
+
 public class PlushieRegistry {
+    private static HashMap<String, String> plushieMap = new HashMap<String, String>();
+
     public static final Block FOX_PLUSHIE = new Block(FabricBlockSettings.create().strength(1.0f));
+    public static final Block PIG_PLUSHIE = new Block(FabricBlockSettings.create().strength(1.0f));
 
     public static void register() {
         registerPlushie("fox_plushie", FOX_PLUSHIE);
+        registerPlushie("pig_plushie", PIG_PLUSHIE);
+        PlushieLootTables.addDropEntries(plushieMap);
     }
 
     public static void registerPlushie(String identifier, Block plushieInstance) {
         Registry.register(Registries.BLOCK, new Identifier(PracticalPlushiesAnimals.MODID, identifier), plushieInstance);
         Registry.register(Registries.ITEM, new Identifier(PracticalPlushiesAnimals.MODID, identifier), new BlockItem(plushieInstance, new FabricItemSettings()));
-        PracticalPlushiesAnimals.LOGGER.info("Hello Fabric world!");
+
+        plushieMap.put(identifier, identifier);
     }
 }

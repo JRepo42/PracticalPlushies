5978e66b6c1094b58bfa91135b543da5e321b677
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-01 16:33:37 +0000

    add(hashmap datagen system working)

--- src/main/java/com/practical_plushies_animals/datagen/PlushieLootTables.java
+++ src/main/java/com/practical_plushies_animals/datagen/PlushieLootTables.java
@@ -4,6 +4,8 @@ import com.practical_plushies_animals.PracticalPlushiesAnimals;
 import com.practical_plushies_animals.registry.PlushieRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 
 import java.util.HashMap;
 
@@ -12,12 +14,17 @@ public class PlushieLootTables extends FabricBlockLootTableProvider {
         super(dataOutput);
     }
 
+    private static HashMap<Block, BlockItem> dataPlushieMap = new HashMap<Block, BlockItem>();
+
     public static void addDropEntries(HashMap plushieMap) {
         PracticalPlushiesAnimals.LOGGER.info(plushieMap.toString());
+        dataPlushieMap = plushieMap;
     }
 
     @Override
     public void generate() {
-        addDrop(PlushieRegistry.FOX_PLUSHIE, PlushieRegistry.FOX_PLUSHIE.asItem());
+        for (Block i : dataPlushieMap.keySet()) {
+            addDrop(i, dataPlushieMap.get(i).asItem());
+        }
     }
 }

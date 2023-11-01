e7b57ec8847d443ced6ec2b77c9edf23ad0749af
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-01 17:00:49 +0000

    rem(unused imports)

--- src/main/java/com/practical_plushies_animals/registry/PlushieRegistry.java
+++ src/main/java/com/practical_plushies_animals/registry/PlushieRegistry.java
@@ -11,7 +11,6 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class PlushieRegistry {
     private static ArrayList<Block> plushies = new ArrayList<>();

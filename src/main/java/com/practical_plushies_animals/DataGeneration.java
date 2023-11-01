9348397c38d252751725f92744565602d20d98ac
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-01 13:59:07 +0000

    Initial commit

---
+++ src/main/java/com/practical_plushies_animals/DataGeneration.java
@@ -0,0 +1,11 @@
+package com.practical_plushies_animals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGeneration implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

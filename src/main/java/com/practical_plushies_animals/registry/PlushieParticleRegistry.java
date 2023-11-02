d8e69a5f8149bcfc22a5f88a74faa7d4225fe651
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-02 16:41:34 +0000

    refactor(particle cleanup)

--- src/main/java/com/practical_plushies_animals/registry/PlushieParticleRegistry.java
+++ src/main/java/com/practical_plushies_animals/registry/PlushieParticleRegistry.java
@@ -22,15 +22,15 @@ public class PlushieParticleRegistry {
     public static final DefaultParticleType TURTLE_PLUSHIE_APPEARANCE = FabricParticleTypes.simple();
 
     public static void register() {
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "allay_plushie_appearance"), ALLAY_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "axolotl_plushie_appearance"), AXOLOTL_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "bee_plushie_appearance"), BEE_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "chicken_plushie_appearance"), CHICKEN_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "cow_plushie_appearance"), COW_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "fox_plushie_appearance"), FOX_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "parrot_plushie_appearance"), PARROT_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "pig_plushie_appearance"), PIG_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "sheep_plushie_appearance"), SHEEP_PLUSHIE_APPEARANCE);
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "turtle_plushie_appearance"), TURTLE_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "allay"), ALLAY_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "axolotl"), AXOLOTL_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "bee"), BEE_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "chicken"), CHICKEN_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "cow"), COW_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "fox"), FOX_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "parrot"), PARROT_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "pig"), PIG_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "sheep"), SHEEP_PLUSHIE_APPEARANCE);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(PracticalPlushiesAnimals.MODID, "turtle"), TURTLE_PLUSHIE_APPEARANCE);
     }
 }

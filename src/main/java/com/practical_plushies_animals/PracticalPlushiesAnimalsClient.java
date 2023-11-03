69b7e4b5348dfc292619fd45eef8839df9f0b0c4
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-03 18:58:47 +0000

    fix(texture transparency not working)

--- src/main/java/com/practical_plushies_animals/PracticalPlushiesAnimalsClient.java
+++ src/main/java/com/practical_plushies_animals/PracticalPlushiesAnimalsClient.java
@@ -2,8 +2,11 @@ package com.practical_plushies_animals;
 
 import com.practical_plushies_animals.particle.PlushieAppearanceParticle;
 import com.practical_plushies_animals.registry.PlushieParticleRegistry;
+import com.practical_plushies_animals.registry.PlushieRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.render.RenderLayer;
 
 public class PracticalPlushiesAnimalsClient implements ClientModInitializer {
     @Override
@@ -18,6 +21,18 @@ public class PracticalPlushiesAnimalsClient implements ClientModInitializer {
         ParticleFactoryRegistry.getInstance().register(PlushieParticleRegistry.PIG_PLUSHIE_APPEARANCE, new PlushieAppearanceParticle.Factory("pig"));
         ParticleFactoryRegistry.getInstance().register(PlushieParticleRegistry.SHEEP_PLUSHIE_APPEARANCE, new PlushieAppearanceParticle.Factory("sheep"));
         ParticleFactoryRegistry.getInstance().register(PlushieParticleRegistry.TURTLE_PLUSHIE_APPEARANCE, new PlushieAppearanceParticle.Factory("turtle"));
+
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.ALLAY_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.AXOLOTL_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.BEE_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.CHICKEN_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.COW_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.FOX_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.PARROT_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.PIG_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.SHEEP_PLUSHIE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(PlushieRegistry.TURTLE_PLUSHIE, RenderLayer.getCutout());
+
     }
 }
 
